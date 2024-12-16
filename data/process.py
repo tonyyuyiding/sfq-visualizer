3b0247ca1b1c49a76eb158bd976928d84f6d82d5
TonyDing <tonyyuyiding@gmail.com>
2024-12-16 15:34:45 +0800

    Delete adjusted statistics

--- data/process.py
+++ data/process.py
@@ -44,15 +44,10 @@ if __name__ == "__main__":
             "Instructor's ITSC",
             "Enrolment",
             "Response Rate",
-            "Response Rate (Adjusted)",
             "Course Overall - Mean",
-            "Course Overall - Mean (Adjusted)",
             "Course Overall - SD",
-            "Course Overall -  SD (Adjusted)",
             "Instructor Overall - Mean",
-            "Instructor Overall - Mean (Adjusted)",
             "Instructor Overall - SD",
-            "Instructor Overall - SD (Adjusted)",
         ]
     )
     # Step 3. Rename columns
@@ -68,15 +63,10 @@ if __name__ == "__main__":
             "Instructor's ITSC": "instructor_itsc",
             "Enrolment": "num_enrollment",
             "Response Rate": "response_rate",
-            "Response Rate (Adjusted)": "response_rate_adj",
             "Course Overall - Mean": "course_mean",
-            "Course Overall - Mean (Adjusted)": "course_mean_adj",
             "Course Overall - SD": "course_sd",
-            "Course Overall -  SD (Adjusted)": "course_sd_adj",
             "Instructor Overall - Mean": "instructor_mean",
-            "Instructor Overall - Mean (Adjusted)": "instructor_mean_adj",
             "Instructor Overall - SD": "instructor_sd",
-            "Instructor Overall - SD (Adjusted)": "instructor_sd_adj",
         }
     )
     # Step 4. Delete unnessary rows
@@ -87,17 +77,11 @@ if __name__ == "__main__":
     # Step 6. Type casting
     df["num_enrollment"] = df["num_enrollment"].apply(pd.to_numeric, errors="coerce")
     df["response_rate"] = df["response_rate"].apply(pd.to_numeric, errors="coerce")
-    df["response_rate_adj"] = df["response_rate_adj"].apply(pd.to_numeric, errors="coerce")
     df["course_mean"] = df["course_mean"].apply(pd.to_numeric, errors="coerce")
-    df["course_mean_adj"] = df["course_mean_adj"].apply(pd.to_numeric, errors="coerce")
     df["course_sd"] = df["course_sd"].apply(pd.to_numeric, errors="coerce")
-    df["course_sd_adj"] = df["course_sd_adj"].apply(pd.to_numeric, errors="coerce")
     df["instructor_mean"] = df["instructor_mean"].apply(pd.to_numeric, errors="coerce")
-    df["instructor_mean_adj"] = df["instructor_mean_adj"].apply(pd.to_numeric, errors="coerce")
     df["instructor_sd"] = df["instructor_sd"].apply(pd.to_numeric, errors="coerce")
-    df["instructor_sd_adj"] = df["instructor_sd_adj"].apply(pd.to_numeric, errors="coerce")
     # Step 7. Calculate the number of responses
     df["num_response"] = round(df["num_enrollment"] * df["response_rate"])
-    df["num_response_adj"] = round(df["num_enrollment"] * df["response_rate_adj"])
-    df.drop(columns=["response_rate", "response_rate_adj"])
+    df.drop(columns=["response_rate"])
     print(df["num_response"])
