895028e8ce20a783b222696d41e2baaa9353f056
TonyDing <tonyyuyiding@gmail.com>
2024-12-25 16:19:10 +0800

    Finish chart data for courses

--- data/analyse.py
+++ data/analyse.py
@@ -28,14 +28,17 @@ def summarize_on_instructors(file_path, save_path=None) -> pd.DataFrame:
         {"mean_times_response_instructor": "sum", "num_response": "sum"}
     )
     df = df.loc[df["num_response"] > 0]
-    df["instructor_mean"] = (df["mean_times_response_instructor"] / df["num_response"]).round(2)
+    df["instructor_mean"] = (
+        df["mean_times_response_instructor"] / df["num_response"]
+    ).round(2)
     df.drop(columns=["mean_times_response_instructor"], inplace=True)
     df = df.loc[df["instructor_mean"] >= 1]
 
     # sorting
     df.sort_values(by=["instructor_mean"], ascending=False, inplace=True)
     df["percentile"] = (
-        df["instructor_mean"].rank(ascending=True, method="max", pct=True).round(4) * 100
+        df["instructor_mean"].rank(ascending=True, method="max", pct=True).round(4)
+        * 100
     )
 
     # add itsc and names
@@ -109,10 +112,10 @@ def chart_data_instructors(file_path, save_path=None) -> dict:
         res_itsc = {}
         for course in courses:
             res_itsc_course = {}
-            df_course = df_filtered.loc[df_filtered["course_code"] == course]
-            semesters = df_course["semester"].unique()
+            df_itsc = df_filtered.loc[df_filtered["course_code"] == course]
+            semesters = df_itsc["semester"].unique()
             for semester in semesters:
-                df_semester: pd.DataFrame = df_course.loc[df_course["semester"] == semester]
+                df_semester: pd.DataFrame = df_itsc.loc[df_itsc["semester"] == semester]
                 df_semester = df_semester.drop(columns=["semester", "course_code"])
                 df_semester = df_semester.loc[df_semester["nr"] > 0]
                 df_semester["cpd"] = df_semester["cm"] * df_semester["nr"]
@@ -139,6 +142,66 @@ def chart_data_instructors(file_path, save_path=None) -> dict:
     return res
 
 
+def chart_data_courses(file_path, save_path=None) -> dict:
+    df = pd.read_csv(file_path)
+
+    df = add_semester(df)
+    df = add_course_code(df)
+    res = {}
+
+    courses = df["course_code"].unique()
+    print("Generating chart data for courses...")
+    for course in tqdm(courses):
+        df_filtered = df.loc[df["course_code"] == course][
+            [
+                "semester",
+                "instructor_itsc",
+                "course_mean",
+                "instructor_mean",
+                "num_response",
+            ]
+        ]
+        df_filtered = df_filtered.rename(
+            columns={
+                "course_mean": "cm",
+                "instructor_mean": "im",
+                "num_response": "nr",
+            }
+        )
+        itscs = df_filtered["instructor_itsc"].unique()
+        res_course = {}
+        for itsc in itscs:
+            res_course_itsc = {}
+            df_itsc = df_filtered.loc[df_filtered["instructor_itsc"] == itsc]
+            semesters = df_itsc["semester"].unique()
+            for semester in semesters:
+                df_semester: pd.DataFrame = df_itsc.loc[df_itsc["semester"] == semester]
+                df_semester = df_semester.drop(columns=["semester", "instructor_itsc"])
+                df_semester = df_semester.loc[df_semester["nr"] > 0]
+                df_semester["cpd"] = df_semester["cm"] * df_semester["nr"]
+                df_semester["ipd"] = df_semester["im"] * df_semester["nr"]
+                df_semester = df_semester.agg({"cpd": "sum", "ipd": "sum", "nr": "sum"})
+                if isinstance(df_semester, pd.Series):
+                    df_semester = df_semester.to_frame().T
+                df_semester.loc[df_semester["cpd"] == 0, "nr"] = 1
+                df_semester["nr"] = df_semester["nr"].astype(int)
+                df_semester["cm"] = (df_semester["cpd"] / df_semester["nr"]).round(2)
+                df_semester["im"] = (df_semester["ipd"] / df_semester["nr"]).round(2)
+                df_semester = df_semester.drop(columns=["cpd", "ipd"])
+                res_course_itsc[semester] = df_semester.to_dict(orient="records")[0]
+
+            res_course[itsc] = res_course_itsc
+
+        res[course] = res_course
+
+    if save_path is not None:
+        print(f"Saving to {save_path}")
+        os.makedirs(os.path.dirname(save_path), exist_ok=True)
+        with open(save_path, "w") as file:
+            json.dump(res, file)
+    return res
+
+
 if __name__ == "__main__":
     # df = summarize_on_instructors(
     #     "data_files/processed/all_processed_data.csv",
@@ -152,6 +215,10 @@ if __name__ == "__main__":
     #     "data_files/processed/all_processed_data.csv",
     #     "data_files/processed/chart_data_instructors.json",
     # )
+    res = chart_data_courses(
+        "data_files/processed/all_processed_data.csv",
+        "data_files/processed/chart_data_courses.json",
+    )
     pass
 
 
@@ -161,3 +228,6 @@ if __name__ == "__main__":
 # chart of each course / instructor
 # declare the time period
 # an option to filter out those with too few responses
+# NOTE: (im == 0 | cm == 0) && nr == 1 means the number is not available
+# NOTE: num_response may not be reliable
+# NOTE: some NaN itsc is in the json files. should ignore them when making website
