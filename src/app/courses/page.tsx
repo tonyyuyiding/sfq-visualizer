ddadce062575798075c047d3ba4a7d3d366d7e39
TonyDing <tonyyuyiding@gmail.com>
2025-01-08 11:31:01 +0800

    Add redirect

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,26 +1,12 @@
-import rankingDataRaw from "../../../data/data_files/processed/ranking_courses.json";
-import { Ranking, RankingItemRaw } from "../components/Ranking";
+import { Suspense } from "react";
+import CourseRanking from "./courseRanking";
 
-const rankingData = JSON.parse(JSON.stringify(rankingDataRaw));
-const rankingItems: RankingItemRaw[] = [];
-
-for (const k in rankingData) {
-    const v = rankingData[k];
-    rankingItems.push({
-        title: k,
-        score: v.course_mean,
-        nr: v.num_response,
-        link: "/courses/" + k.slice(0, 4) + k.slice(5),
-    });
-}
-
-export default function CourseRankings() {
+export default function Page() {
     return (
-        <div>
-            <h1 className="hidden">Course Rankings on SFQ Scores</h1>
-            <Ranking items={rankingItems} searchPrompt="Search for courses here..." scoreName="course_mean" />
-        </div>
-    );
+        <Suspense fallback={<div>Loading...</div>}>
+            <CourseRanking />
+        </Suspense>
+    )
 }
 
 export async function generateMetadata() {
--- src/app/instructors/page.tsx
+++ src/app/instructors/page.tsx
@@ -1,27 +1,12 @@
-import rankingDataRaw from "../../../data/data_files/processed/ranking_instructors.json";
-import { Ranking, RankingItemRaw } from "../components/Ranking";
-import { getNameByItsc } from "../utils";
+import { Suspense } from "react";
+import InstructorRanking from "./instructorRanking";
 
-const rankingData = JSON.parse(JSON.stringify(rankingDataRaw));
-const rankingItems: RankingItemRaw[] = [];
-
-for (const k in rankingData) {
-    const v = rankingData[k];
-    rankingItems.push({
-        title: getNameByItsc(k),
-        score: v.instructor_mean,
-        nr: v.num_response,
-        link: "/instructors/" + k,
-    });
-}
-
-export default function InstructorRankings() {
+export default function Page() {
     return (
-        <div>
-            <h1 className="hidden">Instructor Rankings on SFQ Scores</h1>
-            <Ranking items={rankingItems} searchPrompt="Search for instructor here..." scoreName="instructor_mean" />
-        </div>
-    );
+        <Suspense fallback={<div>Loading...</div>}>
+            <InstructorRanking />
+        </Suspense>
+    )
 }
 
 export async function generateMetadata() {
